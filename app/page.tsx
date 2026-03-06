0204852c45548d56dff9955d4d281f8016daa252
Sinisa <sinisha.stojanovic@gmail.com>
2026-03-06 15:50:18 +0100

    Landing page, Shop, Blog, and Newsletter sections are removed from the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,29 @@
-import HeroSection from '@/components/landing/HeroSection';
-import ServicesSection from '@/components/landing/ServicesSection';
-import AboutSection from '@/components/landing/AboutSection';
-import StatsSection from '@/components/landing/StatsSection';
-import TestimonialsSection from '@/components/landing/TestimonialsSection';
-import FeaturesSection from '@/components/landing/FeaturesSection';
-import TeamSection from '@/components/landing/TeamSection';
-import ShopSection from '@/components/landing/ShopSection';
-import BlogSection from '@/components/landing/BlogSection';
-import NewsletterSection from '@/components/landing/NewsletterSection';
+import HeroSection from "@/components/landing/HeroSection";
+import ServicesSection from "@/components/landing/ServicesSection";
+import AboutSection from "@/components/landing/AboutSection";
+import StatsSection from "@/components/landing/StatsSection";
+import TestimonialsSection from "@/components/landing/TestimonialsSection";
+import FeaturesSection from "@/components/landing/FeaturesSection";
+import TeamSection from "@/components/landing/TeamSection";
+import ShopSection from "@/components/landing/ShopSection";
+import BlogSection from "@/components/landing/BlogSection";
+import NewsletterSection from "@/components/landing/NewsletterSection";
+import GetLicense from "@/components/landing/GetLicense";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <GetLicense />
       <ServicesSection />
       <AboutSection />
       <StatsSection />
       <TestimonialsSection />
       <FeaturesSection />
       <TeamSection />
-      <ShopSection />
-      <BlogSection />
-      <NewsletterSection />
+      {/*<ShopSection />*/}
+      {/*<BlogSection />*/}
+      {/*<NewsletterSection />*/}
     </>
   );
 }
